7a5a135db13b89a3623960fd6ac61056917e694c
Ariq <ariqathllh12@gmail.com>
2024-05-16 14:16:26 +0700

    3.3. Menampilkan gambar dalam grid

--- app/src/main/java/org/d3if3116/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3116/mobpro1/ui/screen/MainScreen.kt
@@ -1,9 +1,15 @@
 package org.d3if3116.mobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -11,23 +17,49 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import org.d3if3116.mobpro1.R
+import org.d3if3116.mobpro1.model.Hewan
+import org.d3if3116.mobpro1.network.HewanApi
 import org.d3if3116.mobpro1.ui.theme.Mobpro1Theme
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
     val viewModel: MainViewModel = viewModel()
-    Column (
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
+    val data by viewModel.data
 
+    LazyVerticalGrid(
+        modifier = modifier.fillMaxSize().padding(4.dp),
+        columns = GridCells.Fixed(2),
+    ) {
+        items(data) { ListItem(hewan = it) }
+    }
+
+    }
+@Composable
+fun ListItem(hewan: Hewan) {
+    Box(
+        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray),
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(HewanApi.getHewanUrl(hewan.imageId))
+                .crossfade(true)
+                .build(),
+            contentDescription = stringResource(R.string.gambar, hewan.nama),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+        )
     }
 }
 
